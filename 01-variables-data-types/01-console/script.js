cb097174a5cb9dae62a8a8c7757dac4282f8509f
Delfin Mongendu <delfmong@Delfins-MacBook-Air.local>
2026-07-08 01:04:08 -0400

    console

--- 01-variables-data-types/01-console/script.js
+++ 01-variables-data-types/01-console/script.js
@@ -1,3 +1,33 @@
+// Log number
 console.log(100);
 
+// Log string
 console.log("Hello World");
+
+// Log multiple values
+console.log(20,'Hello',true)
+
+// Log Variable
+x = 100
+console.log(x);
+
+// Console error & warning
+console.error('Alert');
+console.warn('warning');
+
+// Log object as table
+console.table(
+          {name:'brad',
+           email:'brad@gmail.com'
+        });
+
+// Group console commands
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('warning');
+console.groupEnd();
+
+// add CSS to logs
+const styles = 'padding: 10px; background-color: white; color: green';
+console.log('%cHello World', styles);
